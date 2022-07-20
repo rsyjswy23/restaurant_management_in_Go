46d180a5dd748ab0a8fe230021d2241b3f60d8cd
rsyjswy23 <summerha0211@gmail.com>
2022-07-20 12:58:19 -0700

    completed JWT authentication & added tokens in user models

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"fmt"
+	helper "golang-restaurant-management/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No token provided")})
+			c.Abort()
+			return
+		}
+		// if token exists, then check if invalid or expired
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		// if token is OK, set detailss
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
